acbef12d6ce833c2589da579849e5e98fa43b843
zicoding2 <zaoui.islam2021@gmail.com>
2023-07-13 12:29:09 +0100

    first commit

--- src/routes/error/+page.svelte
+++ src/routes/error/+page.svelte
@@ -3,7 +3,7 @@
 
 <div class="container h-full mx-auto flex flex-col justify-center items-center">
     <div class='flex flex-col variant-ghost-error p-5 justify-center items-center'>
-        <strong class="text-2xl">Unavailable Temporarily</strong>
+        <strong class="text-2xl">Temporarily Unavailable</strong>
     </div>
     
 </div>
