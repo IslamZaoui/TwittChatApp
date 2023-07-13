2b13385266dc11ef0a50223d75ec449e03883e31
zicoding2 <zaoui.islam2021@gmail.com>
2023-07-13 12:42:11 +0100

    first commit

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,4 @@
 import type { PageServerLoad } from './$types';
-import { VERCEL_COMMIT_REF } from '$env/static/private';
 
 export const config = {
     runtime: 'edge',
@@ -7,5 +6,5 @@ export const config = {
 
 export const load = (async (event) => {
     await event.parent()
-    return { git: VERCEL_COMMIT_REF };
+    return { };
 }) satisfies PageServerLoad;
