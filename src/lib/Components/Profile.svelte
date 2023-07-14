7cf6468327fb5e23d163b9cb0f0153da89e56e84
zicoding2 <zaoui.islam2021@gmail.com>
2023-07-14 19:51:25 +0100

    Small Change on Profile page

---
+++ src/lib/Components/Profile.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import { avatarURL } from '$lib/utils';
+	import {
+		Avatar,
+		modalStore,
+		type ModalComponent,
+		type ModalSettings,
+		Modal
+	} from '@skeletonlabs/skeleton';
+	import toast, { Toaster } from 'svelte-french-toast';
+	import ChangeAvatar from './ChangeAvatar.svelte';
+	import type { ActionData } from '../../routes/account/$types';
+	import { faEdit } from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa';
+	import type { PageData } from '../../routes/account/$types';
+
+	export let form: ActionData;
+	export let data: PageData
+
+	$: if (form?.error) {
+		toast.error(form?.error);
+	}
+
+	$: if (form?.pberror) {
+		toast.error(form?.pberror);
+	}
+
+	$: if (form?.success) {
+		toast.success('Avatar uploaded');
+	}
+
+	const modalComponentRegistry: Record<string, ModalComponent> = {
+		modalComponentOne: {
+			ref: ChangeAvatar
+		}
+	};
+	function changeavatar() {
+		const modal: ModalSettings = {
+			type: 'component',
+			component: 'modalComponentOne'
+		};
+		modalStore.trigger(modal);
+	}
+</script>
+
+<Toaster />
+<Modal components={modalComponentRegistry} />
+<div class="flex flex-col">
+	<div class="container mx-auto px-4 py-8">
+		<div class="flex flex-col space-y-5 items-center justify-between mb-4">
+			<div class="flex items-center">
+				<div class="relative inline-block mr-4">
+					<span class="badge-icon variant-filled-warning absolute -top-1 -right-1 z-10">
+						<button on:click={changeavatar}><Fa icon={faEdit} /></button>
+					</span>
+					<Avatar src={avatarURL(data.currentUser.id, data.currentUser.avatar)} initials={data.currentUser.username} width="w-20" />
+				</div>
+				<h1 class="text-xl font-bold">{data.currentUser.username}</h1>
+			</div>
+			<div class="text-sm">
+				Account Status: <span class={!data.currentUser.banned ? 'text-green-500' : 'text-red-500'}
+					>{!data.currentUser.banned ? 'Active' : 'Banned'}</span
+				>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,53 +1,16 @@
 <script lang="ts">
-	import {
-		Avatar,
-		Tab,
-		TabGroup,
-		type ModalSettings,
-		modalStore,
-		Modal,
-		type ModalComponent
-	} from '@skeletonlabs/skeleton';
+	import { Tab, TabGroup } from '@skeletonlabs/skeleton';
 	import type { ActionData, PageData } from './$types';
-	import { faEdit, faGear, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
+	import { faGear, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
-	import ChangeAvatar from '$lib/Components/ChangeAvatar.svelte';
-	import toast, { Toaster } from 'svelte-french-toast';
-	import { avatarURL } from '$lib/utils';
+	import Profile from '$lib/Components/Profile.svelte';
 
 	export let form: ActionData;
 	export let data: PageData;
 
-	$: if (form?.error) {
-		toast.error(form?.error);
-	}
-
-	$: if (form?.pberror) {
-		toast.error(form?.pberror);
-	}
-
-	$: if (form?.success) {
-		toast.success('Avatar uploaded');
-	}
-
-	const user = data.currentUser;
-
 	let tabSet: number = 0;
-	const modalComponentRegistry: Record<string, ModalComponent> = {
-		modalComponentOne: {
-			ref: ChangeAvatar
-		}
-	};
-	function changeavatar() {
-		const modal: ModalSettings = {
-			type: 'component',
-			component: 'modalComponentOne'
-		};
-		modalStore.trigger(modal);
-	}
 </script>
-<Toaster/>
-<Modal components={modalComponentRegistry} />
+
 <TabGroup
 	justify="justify-center"
 	active="variant-filled-primary"
@@ -76,26 +39,7 @@
 	</Tab>
 	<svelte:fragment slot="panel">
 		{#if tabSet === 0}
-			<div class="flex flex-col">
-				<div class="container mx-auto px-4 py-8">
-					<div class="flex flex-col space-y-5 items-center justify-between mb-4">
-						<div class="flex items-center">
-							<div class="relative inline-block mr-4">
-								<span class="badge-icon variant-filled-warning absolute -top-1 -right-1 z-10">
-									<button on:click={changeavatar}><Fa icon={faEdit} /></button>
-								</span>
-								<Avatar src={avatarURL(user.id,user.avatar)} initials={user.username} width="w-20" />
-							</div>
-							<h1 class="text-xl font-bold">{user.username}</h1>
-						</div>
-						<div class="text-sm">
-							Account Status: <span class={!user.banned ? 'text-green-500' : 'text-red-500'}
-								>{!user.banned ? 'Active' : 'Banned'}</span
-							>
-						</div>
-					</div>
-				</div>
-			</div>
+			<Profile data={data} form={form}/>
 		{:else if tabSet === 1}
 			<div class="container h-full mx-auto flex flex-col justify-center items-center">
 				Currently under development
