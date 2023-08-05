fba74497d6d73070d8fafecb709dfad19be60cc7
zicoding2 <zaoui.islam2021@gmail.com>
2023-08-05 02:50:19 +0100

    Brave ADs

--- src/routes/.well-known/+server.ts
+++ src/routes/.well-known/+server.ts
@@ -1,6 +1,8 @@
 import type { RequestHandler } from './$types';
 
+
 export const GET: RequestHandler = async (event) => {
-    const response = await event.fetch('.well-known/brave-rewards-verification.txt');
-    return new Response(await response.text());
+    const response = await event.fetch('./.well-known/brave-rewards-verification.txt');
+    console.log(response)
+    return new Response();
 };
